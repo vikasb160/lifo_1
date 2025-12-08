2db4866a959189dc9a39e1e0ce9d6942bc3f0dc4
vikasb-turing <vikas.b@turing.com>
2025-12-08 21:34:21 +0530

    test update

--- tests/test_lifo_hidden.py
+++ tests/test_lifo_hidden.py
@@ -180,4 +180,4 @@ def test_lifo_hidden_runner():
         hdl_toplevel="lifo",
         always=True,
     )
-    runner.test(hdl_toplevel="lifo", test_module="test_lifo_hidden_runner")
+    runner.test(hdl_toplevel="lifo", test_module="test_lifo_hidden")
